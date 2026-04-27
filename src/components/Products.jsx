d49213308bac5cbd6bf2b8ae01b7ce5715a00ef1
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-27 14:57:17 +0530

    integrating api / backend in redux and complete

---
+++ src/components/Products.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react"
+import { getproducts } from "../store/reducers/ProductReducer"
+import { useDispatch, useSelector } from "react-redux";
+import { asyncgetproducts } from "../store/actions/ProductAction";
+
+const Products = () => {
+
+    const {products} = useSelector((state)=> state.ProductReducer);
+
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(asyncgetproducts());
+    },[]);
+
+
+  return (
+    <div className='m-auto container p-10 mt-5 bg-red-100'>
+      <h1 className='text-2xl font-bold text-red-900 '>Product List</h1> 
+      <ul>
+      {products && products.map((product,index) => {
+        return (
+            <li key={product.id}>
+          <h1>{product.title}{""}
+            <span onClick={()=>DeleteHandler(index)} className='text-red-500 font-black cursor-pointer'>X</span>
+          </h1>
+        </li>
+        )
+      })}
+      </ul>
+    </div>
+  )
+}
+
+export default Products
